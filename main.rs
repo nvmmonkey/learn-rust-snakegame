4cdc11da5546bbc6961ad27cbb18cb12aa12088c
Kit <nvmmonkey@github.com>
2023-11-08 15:13:20 -0800

    Mutable variable: 2 ways

--- main.rs
+++ main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let msg = "Hellow World";
+    //** often used
+    //mutable variable 
+    let mut msg = "Hellow World";
+    println!("{}", msg);
+
+    msg = "Hi there";
     println!("Some text: {}", msg);
+
+    //** not often used
+    //immutable varible double declare is ok in rust
+    let age = 10;
+    println!("{}", age);
+
+    let age = 30;
+    println!("{}", age);
 }
 
 //binary executable code or library
