cfc7c08adf8e4066e49aa17f74e196673ba86ea2
Kit <nvmmonkey@github.com>
2023-11-08 16:40:11 -0800

    Primitive types: tuples, array, iteral array

--- main.rs
+++ main.rs
@@ -1,12 +1,23 @@
 fn main() {
-    //always display decimal
-    let custom_num: i32 = 98_000; //98000
-    let hex_num: i32 = 0xfa; //hexdecimal 16bytes 
-    let bin_num: i32 = 0b0010_1011; //binary 2bytes
-    let byte_num: u8 = b'A'; //UTF encoding A=>0x41 heximal
+    let float_num: f32 = 3.14;
+    let float_num2: f32 = 3.28726387;
 
-    println!("{}", custom_num);
-    println!("{}", hex_num);
-    println!("{}", bin_num);
-    println!("{}", byte_num);
+    //Tuples
+    //A tuple is a collection of values of different types.
+    //Tuples are constructed using parentheses (),
+    // and each tuple itself is a value with type signature (T1, T2, ...),
+    //where T1, T2 are the types of its members.
+    //Functions can use tuples to return multiple values, as tuples can hold any number of values.
+    let tup: (i32, &str, u8) = (20, "Hello", 1);
+
+    println!("{}", tup.1);
+
+    let (a, b, c) = tup;
+    println!("{}", a);
+
+    let x = [1, 3, 5, 7]; //immutable cannot add/delete
+    println!("{}", x[2]); //access array index of 2 => 5
+
+    let y = [2; 6]; // [2,2,2,2,2,2]
+    println!("{}", y[5]); //return 2
 }
