ed70df6b7c71ad7187d1f290b430222e65a4b70e
Kit <nvmmonkey@github.com>
2023-11-08 16:29:09 -0800

    Prime number always display decimals, hexdecimal, binary, bytes, UTF(hex)

--- main.rs
+++ main.rs
@@ -1,23 +1,12 @@
 fn main() {
-    let is_it_fun: bool = true;
+    //always display decimal
+    let custom_num: i32 = 98_000; //98000
+    let hex_num: i32 = 0xfa; //hexdecimal 16bytes 
+    let bin_num: i32 = 0b0010_1011; //binary 2bytes
+    let byte_num: u8 = b'A'; //UTF encoding A=>0x41 heximal
 
-    //i32 -> signed integer of 32bits
-    // -> signed can hold positive or negative value
-    let num1: i32 = -10;
-
-    //u8 -> unsigned integer of 8bits
-    // -> only positive value
-    // HOW many numbers? -> 2^8-1 = 255
-    // u32 -> 2^32-1 postive numbers
-    let small_num1: u8 = 255;
-    let small_num2: u16 = 256;
-
-    // -2^7-1 -> 2^7-1
-    // -128 -> 127
-    let small_num3: i8 = -128;
-
-    // operating system related types
-    let sys_num1: isize = -10;
-    //x32 system usize->u32, x64 usize->u64
-    let sys_num2: usize = 10;
+    println!("{}", custom_num);
+    println!("{}", hex_num);
+    println!("{}", bin_num);
+    println!("{}", byte_num);
 }
