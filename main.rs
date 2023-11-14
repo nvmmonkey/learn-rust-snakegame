54afe039cfd63facb71cf7a7e69be4a4a1d12ae2
Kit <nvmmonkey@github.com>
2023-11-14 14:02:03 -0800

    Stack Overflow, stack example

--- main.rs
+++ main.rs
@@ -1,14 +1,42 @@
-fn main() {
-    let a = 10;
-    let b = a;
-    let c = 15;
-    let d = add(a, b);
+// fn main() {
+//     let a = 10;
+//     let b = a;
+//     let c = 15;
+//     let d = add(a, b);
 
-    println!("{}", d);
+//     println!("{}", d);
+// }
+
+// fn add(x: u32, y: u32) -> u32 {
+//     let sum = x + y;
+//     // return sum;
+//     sum //same as return sum
+// }
+
+fn main() {
+    c();
+    d();
+    f();
 }
 
-fn add(x: u32, y: u32) -> u32 {
-    let sum = x + y;
-    // return sum;
-    sum //same as return sum
+fn a() {
+    println!("calling A");
+    d();
+}
+fn b() {
+    println!("calling B");
+}
+fn c() {
+    println!("calling C");
+}
+fn d() {
+    println!("calling D");
+    a();
+}
+fn e() {
+    println!("calling E");
+}
+fn f() {
+    println!("calling F");
+    b();
 }
