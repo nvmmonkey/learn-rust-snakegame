532c1ccb26462492831f0e5108a2847e8622a689
Kit <nvmmonkey@github.com>
2023-11-14 14:25:04 -0800

    HEAP for string Ptr/Cap/Leg

--- main.rs
+++ main.rs
@@ -1,42 +1,20 @@
-// fn main() {
-//     let a = 10;
-//     let b = a;
-//     let c = 15;
-//     let d = add(a, b);
-
-//     println!("{}", d);
-// }
-
-// fn add(x: u32, y: u32) -> u32 {
-//     let sum = x + y;
-//     // return sum;
-//     sum //same as return sum
-// }
-
 fn main() {
-    c();
-    d();
-    f();
+    // you can place on stack only value with static size
+    let a = 10;
+    let b = a;
+    let c = 15;
+    let d = add(a, b);
+
+    // Store Pointer/Capacity/Length in the Stack Frame
+    // String acutally store on HEAP memory
+    // same as let msg: &str = "Hello";
+    let msg = String::from("Hello");
+
+    println!("{}", msg)
 }
 
-fn a() {
-    println!("calling A");
-    d();
-}
-fn b() {
-    println!("calling B");
-}
-fn c() {
-    println!("calling C");
-}
-fn d() {
-    println!("calling D");
-    a();
-}
-fn e() {
-    println!("calling E");
-}
-fn f() {
-    println!("calling F");
-    b();
+fn add(x: u32, y: u32) -> u32 {
+    let sum = x + y;
+    // return sum;
+    sum //same as return sum
 }
