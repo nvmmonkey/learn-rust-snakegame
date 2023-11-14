1e2c6eed21c329de2a9fc2afe03cbc820c9f149f
Kit <nvmmonkey@github.com>
2023-11-14 13:54:11 -0800

    Stack Frame: //xxd -g1 main for memory access

---
+++ examples.rs
@@ -0,0 +1,6 @@
+fn f() {
+    println!("calling F");
+    b();
+}
+
+//xxd -g1 main
--- main.rs
+++ main.rs
@@ -1,23 +1,14 @@
 fn main() {
-    let float_num: f32 = 3.14;
-    let float_num2: f32 = 3.28726387;
+    let a = 10;
+    let b = a;
+    let c = 15;
+    let d = add(a, b);
 
-    //Tuples
-    //A tuple is a collection of values of different types.
-    //Tuples are constructed using parentheses (),
-    // and each tuple itself is a value with type signature (T1, T2, ...),
-    //where T1, T2 are the types of its members.
-    //Functions can use tuples to return multiple values, as tuples can hold any number of values.
-    let tup: (i32, &str, u8) = (20, "Hello", 1);
+    println!("{}", d);
+}
 
-    println!("{}", tup.1);
-
-    let (a, b, c) = tup;
-    println!("{}", a);
-
-    let x = [1, 3, 5, 7]; //immutable cannot add/delete
-    println!("{}", x[2]); //access array index of 2 => 5
-
-    let y = [2; 6]; // [2,2,2,2,2,2]
-    println!("{}", y[5]); //return 2
+fn add(x: u32, y: u32) -> u32 {
+    let sum = x + y;
+    // return sum;
+    sum //same as return sum
 }
