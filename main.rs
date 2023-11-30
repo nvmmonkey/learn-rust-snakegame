742912b500656257b727ddc071fba9b84efa9774
Kit <nvmmonkey@github.com>
2023-11-30 15:47:59 -0800

    Mutable borrow

--- main.rs
+++ main.rs
@@ -1,12 +1,14 @@
 fn main() {
-    let msg = String::from("Hello");
-    let msg2: &String = &msg;
+    let mut msg = String::from("Hello");
+    let msg2: &mut String = &mut msg;
     // & using reference, not moved data
     // msg2 is not owner of data
     // msg2 is "borrowing" a reference to msg
 
-    println!("{}", msg);
+    msg2.push_str(" World");
+
     println!("{}", msg2);
+    println!("{}", msg);
 }
 
 // msg and msg2 going out of scope
