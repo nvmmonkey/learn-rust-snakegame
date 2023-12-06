54d791fba4c7f1927126929409e0e51c7d297633
Kit <nvmmonkey@github.com>
2023-12-06 15:19:01 -0800

    String slice

--- main.rs
+++ main.rs
@@ -1,7 +1,18 @@
 fn main() {
-   let mut msg = String::from("Hello");
-   let name = "Filip"; // str is read-only memory stack
+    let mut msg = String::from("Hello");
 
-   msg.push_str(" World");
-   name.push_str(" Jerga");
+    let slice = &msg[2..4]; //index 2->3
+    // H E L L O
+    // 0 1[2 3]4
+
+    let slice2 = &msg[2..=4]; //index 2->4
+
+    // H E L L O
+    // 0 1[2 3 4]
+
+    println!("{}", slice);
+    println!("{}", slice.len());
+
+    println!("{}", slice2);
+    println!("{}", slice2.len());
 }
