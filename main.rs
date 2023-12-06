8b4f60133a2f811364823f5431c3b93806171e13
Kit <nvmmonkey@github.com>
2023-12-06 15:22:21 -0800

    String slice mutable/immutable borrowing

--- main.rs
+++ main.rs
@@ -1,18 +1,14 @@
 fn main() {
     let mut msg = String::from("Hello");
-
     let slice = &msg[2..4]; //index 2->3
-    // H E L L O
-    // 0 1[2 3]4
-
-    let slice2 = &msg[2..=4]; //index 2->4
-
-    // H E L L O
-    // 0 1[2 3 4]
+    
+    // Already borrow mutable, so cannot borrow again as immutable
+    // move_me(msg);
+    // msg.clear();
 
     println!("{}", slice);
-    println!("{}", slice.len());
+}
 
-    println!("{}", slice2);
-    println!("{}", slice2.len());
+fn move_me(val:String){
+
 }
