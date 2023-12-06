f88a45fad42776cf77f9b841c3b00ef1097181a5
Kit <nvmmonkey@github.com>
2023-12-06 15:13:13 -0800

    String vs &str

--- main.rs
+++ main.rs
@@ -1,22 +1,7 @@
 fn main() {
-    let a = 10;
-    let b = &a;
-    let mut c = &b;
-    let d = b;
+   let mut msg = String::from("Hello");
+   let name = "Filip"; // str is read-only memory stack
 
-    let e = &&100;
-    c = e;
-
-    println!("Value of c: {:p}", c);
-    println!("Value of e: {:p}", e);
-    println!("Address of 100: {:p}", &(**e));
-
-    println!("Value of c: {:p}", *c);
-    println!("Value of e: {:p}", *e);
-    println!("Address of 100: {:p}", &(**c));
-
-    // println!("Value of a: {:p}", &a);
-    // println!("Value of b: {:p}", b);
-    // println!("Value of c: {:p}", c);
-    // println!("Value of d: {:p}", d);
+   msg.push_str(" World");
+   name.push_str(" Jerga");
 }
